73569e58ee6dcdec387b4edb3498d7015aae4186
Miguel Martelo <miguemartelo1031@gmail.com>
2018-02-26 17:07:33 -0500

    CRUD Hospital

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(bodyParser.json());
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
 var loginRoutes = require('./routes/login');
+var hospitalRoutes = require('./routes/hospital');
 
 
 // Conexión a la base de datos
@@ -32,7 +33,8 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 // Rutas
 app.use('/usuario', usuarioRoutes);
-app.use('/login', loginRoutes)
+app.use('/login', loginRoutes);
+app.use('/hospital', hospitalRoutes);
 app.use('/', appRoutes);
 
 
