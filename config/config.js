fde9dcd074aa9c856d5cdf02f75f0dc45d6fe6d4
MigueMartelo <miguemartelo1031@gmail.com>
2018-03-02 14:41:13 -0500

    Fin de la sección 12 - Backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'esT@-eS-L@-Cl@vE-D3l-T0k3N';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '661332995109-dgfdbklnveibfngk829sbd6cfmknta8r.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET_ID = 'b2i37H5Zdb6vPVgWQyqUKyNN';
