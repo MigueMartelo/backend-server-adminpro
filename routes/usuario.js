9c4aff415e918247644351b662e03bcab05e595c
MigueMartelo <miguemartelo1031@gmail.com>
2018-03-05 16:26:04 -0500

    Actualización de usuario.js y app.js

--- routes/usuario.js
+++ routes/usuario.js
@@ -94,7 +94,7 @@ app.put('/:id', mdAutenticacion.verificaToken, (req, res) => {
 // ================================
 // Crear nuevo usuario
 // ================================
-app.post('/', mdAutenticacion.verificaToken, (req, res) => {
+app.post('/', (req, res) => {
 
 	var body = req.body;
 
